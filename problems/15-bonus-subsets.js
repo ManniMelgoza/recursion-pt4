2b6e25f418b90069683c99362bd1b0e7e4107d4d
Manni <emmanuelmelgoza1@gmail.com>
2024-04-13 16:16:26 -0600

    15 working code, manni will work on his code part later

--- problems/15-bonus-subsets.js
+++ problems/15-bonus-subsets.js
@@ -18,24 +18,37 @@ Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
 
 const subsets = (arr) => {
   // Your code here
-  let uniqueArr = [[]]
-  if(arr.length === 0) return uniqueArr;
+  if(arr.length === 0) return [[]];
 
-  for (let i = 0; i < arr.length; i++){
-    let singleVal = [];
-    singleVal.push(arr[i])
-    // console.log('Values inside singleVal', singleVal)
-    uniqueArr.push(singleVal(arr[i]))
-    arr = arr.slice(i + 1)
-    singleVal.concat(uniqueArr)
-    // // newArr = newArr.concat(flatten(arr[i]));
-    uniqueArr =  uniqueArr.concat(subsets(arr[i] + 1))
+  //EXAMAPLE THAT WORKS
+  const lastElement = arr[arr.length - 1];
+  // console.log('LastElement',lastElement)
+  const subsetsWithoutLast = subsets(arr.slice(0, arr.length - 1));
+  // console.log('subsetsWithoutLast', subsetsWithoutLast)
+  const subsetsWithLast = subsetsWithoutLast.map((subset) => [...subset, lastElement]);
+
+  return subsetsWithoutLast.concat(subsetsWithLast);
   }
-return uniqueArr;
-};
 
-console.log(subsets([])) // [[]]
-console.log(subsets([1])) // [[], [1]]
+   //MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER
+   //MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER
+
+    // let singleVal = [];
+    // singleVal.push(arr[i])
+    // console.log('Single Val after push',singleVal)
+    // console.log('Values inside singleVal', singleVal)
+    // uniqueArr.push(singleVal)
+    // console.log('Unique after pushing singleVal Arr', uniqueArr)
+
+    // arr = arr.slice(i + 1)
+    // singleVal.concat(uniqueArr)
+    // // // newArr = newArr.concat(flatten(arr[i]));
+    // uniqueArr.concat(subsets(arr[i]))
+
+
+
+// console.log(subsets([])) // [[]]
+// console.log(subsets([1])) // [[], [1]]
 console.log(subsets([1, 2])) // [[], [1], [2], [1, 2]]
 // console.log(subsets([1, 2, 3])) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
 
