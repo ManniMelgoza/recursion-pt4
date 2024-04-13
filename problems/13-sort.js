dda4fd729d92533cadee3c06c7bdf55c1b5e8170
Manni <emmanuelmelgoza1@gmail.com>
2024-04-13 11:07:56 -0600

    solving13

--- problems/13-sort.js
+++ problems/13-sort.js
@@ -13,17 +13,27 @@ There are many ways to accomplish this task but here's a simple algorithm:
 3. Add the smallest element to the end of `sorted`
 4. Remove the smallest element from `nums`
 5. Recursively call `sort()` with updated `sorted` and `nums`
-
-Examples:
-
-sort([4,1,6,3,1,7]); // [1, 1, 3, 4, 6, 7]
-sort([0, 1, -3]); // [-3, 0, 1]
-sort([]); // []
 ***********************************************************************/
 
 function sort(nums, sorted = []) {
-    // Your code here 
-}
 
+    // 1. Check the base case: If `nums` is empty, then return `sorted`
+    if (nums.length === 0) return sorted;
+
+    let smallVal = Infinity;
+    for (let i = 0; i < nums.length; i++){
+        if(nums[i] < smallVal){
+            smallVal = nums[i];
+            sorted.push(smallVal)
+            // return sort(nums, sorted)
+        }
+    }
+return sorted;
+};
+
+debugger
+console.log(sort([4,1,6,3,1,7])); // [1, 1, 3, 4, 6, 7]
+console.log(sort([0, 1, -3])); // [-3, 0, 1]
+console.log(sort([])); // []
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = sort;
