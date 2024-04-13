746b5d8f669a6f3e3bded73bc7579df3e80e0e83
DoctorDoofen <and.bennett0930@gmail.com>
2024-04-13 17:26:00 -0500

    update 15

--- problems/15-bonus-subsets.js
+++ problems/15-bonus-subsets.js
@@ -18,39 +18,39 @@ Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
 
 const subsets = (arr) => {
   // Your code here
-  if(arr.length === 0) return [[]];
+  // if (arr.length === 0) return [[]];
 
-  //EXAMAPLE THAT WORKS
-  const lastElement = arr[arr.length - 1];
-  // console.log('LastElement',lastElement)
-  const subsetsWithoutLast = subsets(arr.slice(0, arr.length - 1));
-  // console.log('subsetsWithoutLast', subsetsWithoutLast)
-  const subsetsWithLast = subsetsWithoutLast.map((subset) => [...subset, lastElement]);
+  // //EXAMAPLE THAT WORKS
+  // const lastElement = arr[arr.length - 1];
+  // // console.log('LastElement',lastElement)
+  // const subsetsWithoutLast = subsets(arr.slice(0, arr.length - 1));
+  // // console.log('subsetsWithoutLast', subsetsWithoutLast)
+  // const subsetsWithLast = subsetsWithoutLast.map((subset) => [...subset, lastElement]);
 
-  return subsetsWithoutLast.concat(subsetsWithLast);
-  }
+  // return subsetsWithoutLast.concat(subsetsWithLast);
+}
 
-   //MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER
-   //MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER
+//MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER
+//MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER //MANNI CODE WILL WORK ON LATER
 
-    // let singleVal = [];
-    // singleVal.push(arr[i])
-    // console.log('Single Val after push',singleVal)
-    // console.log('Values inside singleVal', singleVal)
-    // uniqueArr.push(singleVal)
-    // console.log('Unique after pushing singleVal Arr', uniqueArr)
+// let singleVal = [];
+// singleVal.push(arr[i])
+// console.log('Single Val after push',singleVal)
+// console.log('Values inside singleVal', singleVal)
+// uniqueArr.push(singleVal)
+// console.log('Unique after pushing singleVal Arr', uniqueArr)
 
-    // arr = arr.slice(i + 1)
-    // singleVal.concat(uniqueArr)
-    // // // newArr = newArr.concat(flatten(arr[i]));
-    // uniqueArr.concat(subsets(arr[i]))
+// arr = arr.slice(i + 1)
+// singleVal.concat(uniqueArr)
+// // // newArr = newArr.concat(flatten(arr[i]));
+// uniqueArr.concat(subsets(arr[i]))
 
 
-
+debugger
 // console.log(subsets([])) // [[]]
 // console.log(subsets([1])) // [[], [1]]
-console.log(subsets([1, 2])) // [[], [1], [2], [1, 2]]
-// console.log(subsets([1, 2, 3])) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+// console.log(subsets([1, 2])) // [[], [1], [2], [1, 2]]
+console.log(subsets([1, 2, 3, 4])) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
 
 
 
